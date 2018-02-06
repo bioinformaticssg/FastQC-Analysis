be0989647c60c705add2085836fdebe71a8f00e8
Stacey Borrego <sborrego@users.noreply.github.com>
2018-02-06 13:44:10 -0800

    Update 01_fastqc_single_file.sh

--- 01_fastqc_single_file.sh
+++ 01_fastqc_single_file.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#$ -N fastQC                # name of the job
-#$ -o /data/users/$USER/BioinformaticsSG/FastQC/fastqc.out   # contains what would normally be printed to stdout (the$
-#$ -e /data/users/$USER/BioinformaticsSG/FastQC/fastqc.err   # file name to print standard error messages to. These m$
+#$ -N fastQC_01                # name of the job
+#$ -o /data/users/$USER/BioinformaticsSG/FastQC-Analysis/fastqc_01.out   # contains what would normally be printed to stdout (the$
+#$ -e /data/users/$USER/BioinformaticsSG/FastQC-Analysis/fastqc_01.err   # file name to print standard error messages to. These m$
 #$ -q free64,som,asom       # request cores from the free64, som, asom queues.
 #$ -pe openmp 8-64          # request parallel environment. You can include a minimum and maximum core count.
 #$ -m beas                  # send you email of job status (b)egin, (e)rror, (a)bort, (s)uspend
@@ -12,7 +12,7 @@ module load blcr
 module load fastqc/0.11.5   # use the second to latest version, the newest version (0.11.7) does not work at the moment
 
 DATA_DIR=/data/users/$USER/BioinformaticsSG/griffith_data/reads
-QC_OUT_DIR=/data/users/$USER/BioinformaticsSG/FastQC/fastqc_01
+QC_OUT_DIR=/data/users/$USER/BioinformaticsSG/FastQC-Analysis/fastqc_01
 
 FILE="HBR_1_R1.fq.gz"
 
