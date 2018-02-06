342e52a4d7999ad87068f41fcb4fb4b22e38947f
Stacey Borrego <sborrego@users.noreply.github.com>
2018-02-06 13:45:58 -0800

    Update 04_fastqc_html_zip.sh

--- 04_fastqc_html_zip.sh
+++ 04_fastqc_html_zip.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #$ -N fastQC_04                # name of the job
-#$ -o /data/users/$USER/BioinformaticsSG/FastQC/fastqc.out   # contains what would normally be printed to stdout (the$
-#$ -e /data/users/$USER/BioinformaticsSG/FastQC/fastqc.err   # file name to print standard error messages to. These m$
+#$ -o /data/users/$USER/BioinformaticsSG/FastQC-Analysis/fastqc.out   # contains what would normally be printed to stdout (the$
+#$ -e /data/users/$USER/BioinformaticsSG/FastQC-Analysis/fastqc.err   # file name to print standard error messages to. These m$
 #$ -q free64,som,asom       # request cores from the free64, som, asom queues.
 #$ -pe openmp 8-64          # request parallel environment. You can include a minimum and maximum core count.
 #$ -m beas                  # send you email of job status (b)egin, (e)rror, (a)bort, (s)uspend
@@ -12,8 +12,8 @@ module load blcr
 module load fastqc/0.11.5   # use the second to latest version, the newest version (0.11.7) does not work at the moment
 
 DATA_DIR=/data/users/$USER/BioinformaticsSG/griffith_data/reads
-QC_OUT_DIR=/data/users/$USER/BioinformaticsSG/FastQC/fastqc_results_04
-QC_HTML_DIR=/data/users/$USER/BioinformaticsSG/FastQC/fastqc_results_04/fastqc_html_04
+QC_OUT_DIR=/data/users/$USER/BioinformaticsSG/FastQC-Analysis/fastqc_results_04
+QC_HTML_DIR=/data/users/$USER/BioinformaticsSG/FastQC-Analysis/fastqc_results_04/fastqc_html_04
 
 mkdir -p ${QC_OUT_DIR}
 mkdir -p ${QC_HTML_DIR}
