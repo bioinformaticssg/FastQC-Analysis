b0a85c3592631a6ca361436aaafd525c7fec4102
Stacey Borrego <sborrego@users.noreply.github.com>
2018-02-06 13:44:39 -0800

    Update 02_fastqc_multi.sh

--- 02_fastqc_multi.sh
+++ 02_fastqc_multi.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #$ -N fastQC_02              # name of the job
-#$ -o /data/users/$USER/BioinformaticsSG/FastQC/fastqc_02.out   # contains what would normally be printed to stdout (the$
-#$ -e /data/users/$USER/BioinformaticsSG/FastQC/fastqc_02.err   # file name to print standard error messages to. These m$
+#$ -o /data/users/$USER/BioinformaticsSG/FastQC-Analysis/fastqc_02.out   # contains what would normally be printed to stdout (the$
+#$ -e /data/users/$USER/BioinformaticsSG/FastQC-Analysis/fastqc_02.err   # file name to print standard error messages to. These m$
 #$ -q free64,som,asom       # request cores from the free64, som, asom queues.
 #$ -pe openmp 8-64          # request parallel environment. You can include a minimum and maximum core count.
 #$ -m beas                  # send you email of job status (b)egin, (e)rror, (a)bort, (s)uspend
@@ -12,13 +12,11 @@ module load blcr
 module load fastqc/0.11.5   # use the second to latest version, the newest version (0.11.7) does not work at the moment
 
 DATA_DIR=/data/users/$USER/BioinformaticsSG/griffith_data/reads
-QC_OUT_DIR=/data/users/$USER/BioinformaticsSG/FastQC/fastqc_results_02
+QC_OUT_DIR=/data/users/$USER/BioinformaticsSG/FastQC-Analysis/fastqc_results_02
 
 mkdir -p ${QC_OUT_DIR}
 
 for FILE in `find ${DATA_DIR} -name \*`; do 
-    
     fastqc $FILE \
     -o ${QC_OUT_DIR}
-    
 done
