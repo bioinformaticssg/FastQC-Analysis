96b87c5847f5adb9973a3d802c5c30feea70e41c
Bioinformatics Support Group <35555041+bioinformaticssg@users.noreply.github.com>
2018-02-06 13:35:02 -0800

    Create 04_fastqc_html_zip.sh

---
+++ 04_fastqc_html_zip.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#$ -N fastQC_04                # name of the job
+#$ -o /data/users/$USER/BioinformaticsSG/FastQC/fastqc.out   # contains what would normally be printed to stdout (the$
+#$ -e /data/users/$USER/BioinformaticsSG/FastQC/fastqc.err   # file name to print standard error messages to. These m$
+#$ -q free64,som,asom       # request cores from the free64, som, asom queues.
+#$ -pe openmp 8-64          # request parallel environment. You can include a minimum and maximum core count.
+#$ -m beas                  # send you email of job status (b)egin, (e)rror, (a)bort, (s)uspend
+#$ -ckpt blcr               # (c)heckpoint: writes a snapshot of a process to disk, (r)estarts the process after the checkpoint is c$
+
+module load blcr
+module load fastqc/0.11.5   # use the second to latest version, the newest version (0.11.7) does not work at the moment
+
+DATA_DIR=/data/users/$USER/BioinformaticsSG/griffith_data/reads
+QC_OUT_DIR=/data/users/$USER/BioinformaticsSG/FastQC/fastqc_results_04
+QC_HTML_DIR=/data/users/$USER/BioinformaticsSG/FastQC/fastqc_results_04/fastqc_html_04
+
+mkdir -p ${QC_OUT_DIR}
+mkdir -p ${QC_HTML_DIR}
+
+for FILE in `find ${DATA_DIR} -name \*`; do 
+    fastqc $FILE \
+    -o ${QC_OUT_DIR}
+    
+    mv ${QC_OUT_DIR}/*.html ${QC_HTML_DIR}
+done
+
+tar -C ${QC_OUT_DIR} -czvf ${QC_HTML_DIR}.tar.gz ${QC_HTML_DIR}/*
