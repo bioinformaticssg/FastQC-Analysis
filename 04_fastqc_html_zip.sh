18fc620c4fa279c8ff90c7057ef6d7f0fc3283d1
Stacey Borrego <sborrego@users.noreply.github.com>
2018-02-07 13:18:41 -0800

    Update 04_fastqc_html_zip.sh

--- 04_fastqc_html_zip.sh
+++ 04_fastqc_html_zip.sh
@@ -9,7 +9,7 @@
 #$ -ckpt blcr               # (c)heckpoint: writes a snapshot of a process to disk, (r)estarts the process after the checkpoint is c$
 
 module load blcr
-module load fastqc/0.11.5   # use the second to latest version, the newest version (0.11.7) does not work at the moment
+module load fastqc/0.11.7
 
 DATA_DIR=/data/users/$USER/BioinformaticsSG/griffith_data/reads
 QC_OUT_DIR=/data/users/$USER/BioinformaticsSG/FastQC-Analysis/fastqc_results_04
@@ -19,14 +19,19 @@ HTML="fastqc_html_04"
 mkdir -p ${QC_OUT_DIR}          # Making the result file directory
 mkdir -p ${QC_HTML_DIR}         # Making the HTML result file directory
 
-for FILE in `find ${DATA_DIR} -name \*`; do         # Performing a loop that will use each file in the data directory, "*" is a wild card symbol and in this context matches any file in the indicated directory
-    fastqc $FILE \                                  # Each file will be processed with the program "fastqc", "\" symbol indicates that more options for the program are on the next line
-    -o ${QC_OUT_DIR}                                # This indicates the output directory for the result files
+# Here we are performing a loop that will use each file in our data directory as input, "*" is a wild card symbol and in this context matches any file in the indicated directory
+# Each file will be processed with the program "fastqc", "\" symbol indicates that more options for the program are on the next line 
+# (--outdir) indicates the output directory for the result files
+# (mv) moves the HTML result files to the new HTML result file directory
+
+for FILE in `find ${DATA_DIR} -name \*`; do
+    fastqc $FILE \
+    -o ${QC_OUT_DIR}
     
-    mv ${QC_OUT_DIR}/*.html ${QC_HTML_DIR}          # Moves the HTML result files to the new HTML result file directory
+    mv ${QC_OUT_DIR}/*.html ${QC_HTML_DIR}
 done
 
-tar -C ${QC_OUT_DIR} -czvf ${HTML}.tar.gz ${HTML}   # Compresses the HTML result file. 
-                                                    # -C flag prevents the parent directories from being included in the archive
-                                                    # -csvf (c)reate archive, use g(z)ip for compression, (v)erbosely shows the .tar file progress, (f)ilename appears next in the command
- 
+# Here we are compressing the HTML result file using the program tar
+# -C flag prevents the parent directories from being included in the archive
+# -csvf (c)reates archive, uses g(z)ip for compression, (v)erbosely shows the .tar file progress, (f)ilename appears next in the command
+tar -C ${QC_OUT_DIR} -czvf ${HTML}.tar.gz ${HTML}
